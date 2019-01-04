c5bd644571223376d0fa83a5575ebb12038713a4
DavidDai <185098535@qq.com>
2019-01-04 16:35:51 +0800

    学习ts类相关的操作

--- class.js
+++ class.js
@@ -117,4 +117,153 @@ var animal = new AnimalV3("Goat");
 var rhino = new Rhino();
 var employee = new Employee("Bob");
 animal = rhino;
-animal = employee;
+// animal = employee 'Employee' is not assignable to type 'AnimalV3'. Types have separate declarations of a private property 'name'.
+// 理解 protected 修饰符与 private修饰符的行为很相似，但有一点不同， protected成员在派生类中仍然可以访问。
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    return Person;
+}());
+var Employee1 = /** @class */ (function (_super) {
+    __extends(Employee1, _super);
+    function Employee1(name, department) {
+        var _this = _super.call(this, name) || this;
+        _this.department = department;
+        return _this;
+    }
+    Employee1.prototype.getElevatorPitch = function () {
+        return "Hello, my name is " + this.name + " and I work in " + this.department;
+    };
+    return Employee1;
+}(Person));
+var howard = new Employee1('Howard', 'Sales');
+console.log(howard.getElevatorPitch());
+// console.log(howard.name) 注意，我们不能在 Person类外使用 name，但是我们仍然可以通过 Employee类的实例方法访问，因为 Employee是由 Person派生而来的。
+// 构造函数也可以被标记成 protected。 这意味着这个类不能在包含它的类外被实例化，但是能被继承
+var Person1 = /** @class */ (function () {
+    function Person1(theName) {
+        this.name = theName;
+    }
+    return Person1;
+}());
+// Employee2 能够继承 Person1
+var Employee2 = /** @class */ (function (_super) {
+    __extends(Employee2, _super);
+    function Employee2(name, department) {
+        var _this = _super.call(this, name) || this;
+        _this.department = department;
+        return _this;
+    }
+    Employee2.prototype.getElevatorPitch = function () {
+        return "Hello, my name is " + this.name + " and I work in " + this.department + ".";
+    };
+    return Employee2;
+}(Person1));
+var howard1 = new Employee2('Howard', 'Sales');
+// let join = new Person1('John') // 错误: 'Person1' 的构造函数是被保护的.
+// readonly修饰符
+var Octopus = /** @class */ (function () {
+    function Octopus(theName) {
+        this.numberOfLegs = 8;
+        this.name = theName;
+    }
+    return Octopus;
+}());
+var dad = new Octopus('Man with the 8 strong legs');
+// dad.name = 'Man with the 3-prece suit'   错误  name  是只读的
+// 参数属性
+var Octopus1 = /** @class */ (function () {
+    function Octopus1(name) {
+        this.name = name;
+        this.numberOfLegs = 8;
+    }
+    return Octopus1;
+}());
+// 存取器  TypeScript支持通过getters/setters来截取对对象成员的访问。 它能帮助你有效的控制对对象成员的访问。 下面来看如何把一个简单的类改写成使用 get和 set。 首先，我们从一个没有使用存取器的例子开始。
+var Employee3 = /** @class */ (function () {
+    function Employee3() {
+    }
+    return Employee3;
+}());
+var employee3 = new Employee3();
+employee3.fullName = "Bob Smith";
+employee3.fullName && console.log(employee3.fullName);
+// 拦截get 和 setter
+var passcode = "secret passcode";
+var Employee4 = /** @class */ (function () {
+    function Employee4() {
+    }
+    Object.defineProperty(Employee4.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (passcode && passcode === "secret passcode") {
+                this._fullName = newName;
+            }
+            else {
+                console.log("Error: Unauthorized update of employee!");
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employee4;
+}());
+var employee4 = new Employee4();
+employee4.fullName = "Bob Smith";
+if (employee4.fullName) {
+    alert(employee4.fullName);
+}
+// 静态属性
+var Grid = /** @class */ (function () {
+    function Grid(scale) {
+        this.scale = scale;
+    }
+    Grid.prototype.calc = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    };
+    Grid.origin = {
+        x: 0,
+        y: 0
+    };
+    return Grid;
+}());
+var grid1 = new Grid(1.0);
+var grid2 = new Grid(5.0);
+// 抽象类
+var Animal4 = /** @class */ (function () {
+    function Animal4() {
+    }
+    Animal4.prototype.moveBy = function () {
+        console.log('roaming the earch...');
+    };
+    return Animal4;
+}());
+// 抽象类中的抽象方法不包含具体实现并且必须在派生类中实现。 抽象方法的语法与接口方法相似。 两者都是定义方法签名但不包含方法体。 然而，抽象方法必须包含 abstract关键字并且可以包含访问修饰符。
+var Department = /** @class */ (function () {
+    function Department(name) {
+        this.name = name;
+    }
+    Department.prototype.printName = function () {
+        console.log('Department name: ' + this.name);
+    };
+    return Department;
+}());
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment() {
+        return _super.call(this, 'Accounting and Auditing') || this; // 在派生类的构造函数中必须调用 super()
+    }
+    AccountingDepartment.prototype.printMeeting = function () {
+        console.log('The Accounting Department meets each Monday at 10am.');
+    };
+    AccountingDepartment.prototype.generateReports = function () {
+        console.log('Generating accounting reports...');
+    };
+    return AccountingDepartment;
+}(Department));
+var department; // 允许创建一个对抽象类型的引用
--- class.ts
+++ class.ts
@@ -110,6 +110,144 @@ let rhino = new Rhino()
 let employee = new Employee("Bob")
 
 animal = rhino
-animal = employee
+// animal = employee 'Employee' is not assignable to type 'AnimalV3'. Types have separate declarations of a private property 'name'.
 
-// 理解 protected
+// 理解 protected 修饰符与 private修饰符的行为很相似，但有一点不同， protected成员在派生类中仍然可以访问。
+class Person {
+  protected name: string
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+class Employee1 extends Person {
+  private department: string
+  constructor(name: string, department: string) {
+    super(name)
+    this.department = department
+  }
+  public getElevatorPitch () {
+    return `Hello, my name is ${this.name} and I work in ${this.department}`
+  }
+}
+
+let howard = new Employee1 ('Howard', 'Sales')
+console.log(howard.getElevatorPitch())
+// console.log(howard.name) 注意，我们不能在 Person类外使用 name，但是我们仍然可以通过 Employee类的实例方法访问，因为 Employee是由 Person派生而来的。
+
+
+// 构造函数也可以被标记成 protected。 这意味着这个类不能在包含它的类外被实例化，但是能被继承
+class Person1 {
+  protected name: string
+  protected constructor (theName: string) {
+    this.name = theName
+  }
+}
+// Employee2 能够继承 Person1
+class Employee2 extends Person1 {
+  private department: string
+  constructor(name: string, department: string) {
+    super(name)
+    this.department = department
+  }
+
+  public getElevatorPitch () {
+    return `Hello, my name is ${this.name} and I work in ${this.department}.`
+  }
+}
+let howard1 = new Employee2('Howard', 'Sales')
+// let join = new Person1('John') // 错误: 'Person1' 的构造函数是被保护的.
+
+// readonly修饰符
+class Octopus {
+  readonly name: string
+  readonly numberOfLegs: number = 8
+  constructor (theName: string) {
+    this.name = theName
+  }
+}
+let dad = new Octopus('Man with the 8 strong legs')
+// dad.name = 'Man with the 3-prece suit'   错误  name  是只读的
+
+// 参数属性
+class Octopus1 {
+  readonly numberOfLegs: number = 8
+  constructor (readonly name: string) {} 
+}
+
+// 存取器  TypeScript支持通过getters/setters来截取对对象成员的访问。 它能帮助你有效的控制对对象成员的访问。 下面来看如何把一个简单的类改写成使用 get和 set。 首先，我们从一个没有使用存取器的例子开始。
+class Employee3 {
+  fullName: string
+}
+
+let employee3 = new Employee3()
+employee3.fullName = "Bob Smith"
+employee3.fullName && console.log(employee3.fullName)
+
+// 拦截get 和 setter
+let passcode = "secret passcode"
+class Employee4 {
+  private _fullName: string
+  get fullName (): string {
+    return this._fullName
+  }
+  set fullName (newName: string) {
+    if (passcode && passcode === "secret passcode") {
+      this._fullName = newName
+    } else {
+      console.log("Error: Unauthorized update of employee!")
+    }
+  }
+}
+let employee4 = new Employee4()
+employee4.fullName = "Bob Smith"
+if (employee4.fullName) {
+  alert(employee4.fullName)
+}
+
+// 静态属性
+class Grid {
+  static origin = {
+    x: 0,
+    y: 0
+  }
+  calc (point: {x: number, y: number}): number {
+    let xDist = (point.x - Grid.origin.x)
+    let yDist = (point.y - Grid.origin.y)
+    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale
+  }
+  constructor (public scale: number) {}
+}
+let grid1 = new Grid(1.0)
+let grid2 = new Grid(5.0)
+
+// 抽象类
+abstract class Animal4 {
+  abstract maskeSound(): void
+  moveBy (): void {
+    console.log('roaming the earch...')
+  }
+}
+
+// 抽象类中的抽象方法不包含具体实现并且必须在派生类中实现。 抽象方法的语法与接口方法相似。 两者都是定义方法签名但不包含方法体。 然而，抽象方法必须包含 abstract关键字并且可以包含访问修饰符。
+abstract class Department {
+  constructor (public name: string) {}
+  printName(): void {
+    console.log('Department name: ' + this.name)
+  }
+  abstract printMeeting(): void   // 必须在派生类中实现
+}
+
+class AccountingDepartment extends Department {
+  constructor () {
+    super('Accounting and Auditing') // 在派生类的构造函数中必须调用 super()
+  }
+  printMeeting (): void {
+    console.log('The Accounting Department meets each Monday at 10am.')
+  }
+  generateReports(): void {
+    console.log('Generating accounting reports...')
+  }
+}
+
+let department: Department // 允许创建一个对抽象类型的引用
