8d269ff3f7ca11e2a2e794aa683fe2fc292d4af7
DavidDai <185098535@qq.com>
2019-01-25 18:26:09 +0800

    泛型学习

--- fanxing.js
+++ fanxing.js
@@ -17,7 +17,56 @@ function loggerIdentity(arg) {
     // console.log(arg.length)   // Property 'length' does not exist on type 'T'.
     return arg;
 }
+// 由于我们操作的是数组，所以.length属性是应该存在的。 我们可以像创建其它数组一样创建这个数组：
 function loggerIdentity1(arg) {
     console.log(arg.length);
     return arg;
 }
+// 泛型类型
+function identity3(arg) {
+    return arg;
+}
+var myIdentity3 = identity3;
+// 我们也可以使用不同的泛型参数名，只要在数量上和使用方式上能对应上就可以
+function identity4(arg) {
+    return arg;
+}
+var myIdentity4 = identity4;
+function identity5(arg) {
+    return arg;
+}
+var myIdentity5 = identity5;
+// 泛型类
+var GenericNumber = /** @class */ (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+// GenericNumber类的使用是十分直观的，并且你可能已经注意到了，没有什么去限制它只能使用number类型。 也可以使用字符串或其它更复杂的类型。
+var stringNumeric = new GenericNumber();
+stringNumeric.zeroValue = '';
+stringNumeric.add = function (x, y) { return x + y; };
+console.log(stringNumeric.add(stringNumeric.zeroValue, 'test'));
+// 泛型约束
+function loggingIdent(arg) {
+    // console.log(arg.length) Property 'length' does not exist on type 'T'.
+    return arg;
+}
+function loggingIdent1(arg) {
+    console.log(arg.length);
+    return arg;
+}
+// function loggingIdent2<T extends Lengthwise> (arg: T): T {
+//   console.log(arg.length)
+//   return arg
+// }
+// loggingIdent1(3);  Argument of type '3' is not assignable to parameter of type '{}[]'.
+function getProperty(obj, key) {
+    return obj[key];
+}
+var x = { a: 1, b: 2, c: 3, d: 4 };
+getProperty(x, "a"); // okay
+getProperty(x, "m"); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd'.
--- fanxing.ts
+++ fanxing.ts
@@ -30,3 +30,68 @@ function loggerIdentity1<T>(arg: T[]): T[] {
 }
 
 // 泛型类型
+function identity3<T> (arg: T): T  {
+  return arg
+}
+
+let myIdentity3: {<T>(arg:T): T} = identity3
+
+// 我们也可以使用不同的泛型参数名，只要在数量上和使用方式上能对应上就可以
+
+function identity4<T>(arg: T): T {
+  return arg
+}
+
+let myIdentity4: <U>(arg: U) => U = identity4
+
+// 我们还可以使用带有调用签名的对象字面量来定义泛型函数：
+interface GenericFn <T> {
+  (arg: T): T
+}
+
+function identity5<T>(arg: T): T {
+  return arg
+}
+
+let myIdentity5: GenericFn<number> = identity5
+
+// 泛型类
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x: T, y: T) => T
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0
+myGenericNumber.add = function(x, y) {return x + y}
+// GenericNumber类的使用是十分直观的，并且你可能已经注意到了，没有什么去限制它只能使用number类型。 也可以使用字符串或其它更复杂的类型。
+
+let stringNumeric = new GenericNumber<string>();
+stringNumeric.zeroValue = ''
+stringNumeric.add = function (x, y) {return x + y}
+console.log(stringNumeric.add(stringNumeric.zeroValue, 'test'))
+
+// 泛型约束
+function loggingIdent<T>(arg: T): T {
+  // console.log(arg.length) Property 'length' does not exist on type 'T'.
+  return arg
+}
+
+function loggingIdent1<T>(arg: T[]): T[] {
+  console.log(arg.length)
+  return arg
+}
+
+// function loggingIdent2<T extends Lengthwise> (arg: T): T {
+//   console.log(arg.length)
+//   return arg
+// }
+// loggingIdent1(3);  Argument of type '3' is not assignable to parameter of type '{}[]'.
+
+// function getProperty(obj: T, key: K) {
+//   return obj[key]
+// }
+// let x = {a: 1, b: 2, c: 3, d: 4}
+// getProperty(x, 'a')
+// getProperty(x, 'm')
+
